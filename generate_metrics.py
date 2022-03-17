f6e2b87262d01de8eb930d8d56d739b51c3e6f08
Leonardo Germán Loza Bonora <lozabonora@gmail.com>
2022-03-17 09:32:47 -0300

    refactor generate_metrics

--- generate_metrics.py
+++ generate_metrics.py
@@ -1,15 +1,49 @@
-import json 
 from pymongo import MongoClient
-import ast
-import os
 from pathlib import Path
 import special_case
 import clean_data
+import json
+import ast
+import os
+
+
+def make_query(node_name, query_graph, started_queries, maked_queries, db):
+    if (node_name in maked_queries):
+        return False
+
+    if (node_name in started_queries):
+        return True
+    started_queries.append(node_name)
+
+    # chequeo de si es una collection de otra pipeline
+    if (node_name not in query_graph):
+        if (node_name not in db.list_collection_names()):
+            return True
+        maked_queries.append(node_name)
+        return False
+
+    needed_coll = query_graph[node_name]['dependsOf']
+    needed_coll.append(query_graph[node_name]['forCollection'])
+
+    for query_name in needed_coll:
+        circular_reference = make_query(query_name, query_graph, started_queries, maked_queries, db)
+        if (circular_reference):
+            return True
+    
+    # hace la query en mongodb
+    col = db[query_graph[node_name]['forCollection']]
+    col.aggregate(query_graph[node_name]['pipeline'], allowDiskUse=True)
+
+    maked_queries.append(node_name)
+    return False
 
 def execute_queries(mongo_client, db_name, directory_name):
     db = mongo_client[db_name]
 
     directory = os.path.join(Path().absolute(), directory_name)
+
+    query_graph = {}
+    all_coll = db.list_collection_names()
 
     for filename in os.listdir(directory):
         if not filename.endswith(".json"):
@@ -18,22 +52,23 @@ def execute_queries(mongo_client, db_name, directory_name):
         with open(os.path.join(directory, filename), encoding="utf-8") as file:
             query = json.load(file)
 
-            # Chequea que existan las colecciones necesarias
-            needed_coll = query['dependsOf']
-            needed_coll.append(query['forCollection'])
-            all_coll = db.list_collection_names()
-            if not (all(coll in all_coll for coll in needed_coll)):
-                continue
-
             with open (os.path.join(directory, query['name']), 'r') as fp:
                 pipeline = fp.read()
 
                 # Transforma de string a lista con objetos
                 pipeline = ast.literal_eval(pipeline)
 
-                # Ejecuta la query en la coleccion
-                col = db[query['forCollection']]
-                col.aggregate(pipeline, allowDiskUse=True)
+                query['pipeline'] = pipeline
+
+            query_graph[query['name']] = query
+    
+    started_queries = []
+    maked_queries = []
+    for node_name in query_graph:
+        circular_reference = make_query(node_name, query_graph, started_queries, maked_queries, db)
+        if (circular_reference):
+            print("Se corto ejecución por problema")
+            break
 
 def main():
     client = MongoClient('localhost')
